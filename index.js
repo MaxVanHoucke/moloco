62d85cedbc25e8d3df81b9f50a6370f856bd1c06
Max Van Houcke <max.vanhoucke@student.uantwerpen.be>
2021-10-10 00:56:43 +0200

    Animation centered in the middle

--- index.js
+++ index.js
@@ -4,23 +4,35 @@ new fullpage('#fullpage', {
 });
 
 
-/**
- * Animation
- */
-document.addEventListener('scroll', function (e) {
+function onScroll() {
     const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     const windowHeight = window.innerHeight;
     const images = document.querySelectorAll('#animation img');
 
+    const totalAnimationHeight = Array.from(images).reduce((acc, element) => acc + element.clientHeight, 0);
+    const startTop = Math.floor(windowHeight / 2 - totalAnimationHeight / 2);
+
     // Set height of first ball
     const ballHeight = Math.max(300 - winScroll, 72);
+    const startBallTop = Math.floor(windowHeight / 2 - ballHeight / 2);
     images[0].style.height = ballHeight.toString() + 'px';
 
     // Set top of all elements
+    let animationHeight = 0;
     for (let i = 0; i < images.length; i++) {
         const element = images[i];
-        const elementTop = parseInt(element.dataset.h);
-        element.style.top = Math.max(i * windowHeight + elementTop - winScroll, elementTop).toString() + 'px'
+        if (i === 0) {
+            // Needs a drawing to make sense
+            element.style.top = Math.min(startBallTop, startTop + windowHeight - Math.min(windowHeight, winScroll)).toString() + 'px';
+        } else {
+            element.style.top = Math.max(i * windowHeight + animationHeight + startTop - winScroll, animationHeight + startTop).toString() + 'px'
+        }
+        animationHeight += element.clientHeight;
     }
-});
+}
 
+/**
+ * Animation
+ */
+document.addEventListener('scroll', onScroll);
+onScroll();
